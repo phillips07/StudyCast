336c52bcabcda65f2bc91795840e4bb336c9210d
Austin Phillips <austin.e.phillips@gmail.com>
2016-11-06 09:56:25 -0800

    Table populates and ensures the same class cannot be added twice

--- StudyCast/ClassSelectController+handlers.swift
+++ StudyCast/ClassSelectController+handlers.swift
@@ -20,7 +20,38 @@ extension ClassSelectController {
     
     }
     
-    func handleTap(){
+    func handleTap(recognizer: UITapGestureRecognizer){
+
+        if recognizer.state == UIGestureRecognizerState.ended {
+            let tapLocation = recognizer.location(in: self.facultyTableView)
+            if let tapIndexPath = self.facultyTableView.indexPathForRow(at: tapLocation) {
+                if let tappedCell = self.facultyTableView.cellForRow(at: tapIndexPath) as? UserCell {
+                    
+                    var i = numCells - 1
+                    var added = false
+                    while i >= 0 {
+                        if ((tappedCell.textLabel?.text)! == pickedClassesDataSet[i]) {
+                            added = true
+                            break
+                        }
+                        i -= 1
+                    }
+                    
+                    if !added {
+                        print("****************************")
+                    
+                        numCells += 1
+                        print(numCells)
+                        print((tappedCell.textLabel?.text)!)
+                        pickedClassesDataSet.append((tappedCell.textLabel?.text)!)
+                    
+                        print(pickedClassesDataSet)
+                        userClassesTableView.reloadData()
+                        
+                    }
+                }
+            }
+        }
         
     }
     
