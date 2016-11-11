a51d531146d2925daa92d6a9d70c3855ec2c7811
dennishuebert <dennishuebert1@gmail.com>
2016-11-11 15:03:03 -0800

    Added tabBar to the main view

---
+++ StudyCast/CustomTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  CustomTabBarController.swift
+//  StudyCast
+//
+//  Created by Dennis Huebert on 2016-11-11.
+//  Copyright © 2016 Austin Phillips. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let friendsContoller = UIViewController()
+        let friendsNavigationController = UINavigationController(rootViewController: friendsContoller)
+        friendsNavigationController.title = "Friends"
+        //add icon for friends here
+        //friendsNavigationController.tabBarItem.image = UIImage(named: "friends_icon")
+        
+        //change castContoller to different view later
+        let castController = UIViewController()
+        let castNavigationController = UINavigationController(rootViewController: castController)
+        castNavigationController.title = "Cast"
+        //add icon for cast here
+        //castNavigationController.tabBarItem.image = UIImage(named: "cast_icon")
+        
+        let homeController = MainScreenController()
+        let homeNavigationController = UINavigationController(rootViewController: homeController)
+        homeNavigationController.title = "Home"
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.white], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGray], for: .selected)
+        
+        viewControllers = [homeNavigationController, castNavigationController, friendsNavigationController]
+    }
+}
--- StudyCast/MainScreenController.swift
+++ StudyCast/MainScreenController.swift
@@ -9,13 +9,15 @@
 import UIKit
 import Firebase
 
-class MainScreenController: UITabBarController {
+class MainScreenController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
-        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: .plain, target: self, action: #selector(handleSettings))
+        self.navigationController?.navigationBar.barTintColor = UIColor(r: 61, g: 91, b: 151)
+        self.tabBarController?.tabBar.barTintColor = UIColor(r: 61, g: 91, b: 151)
         if FIRAuth.auth()?.currentUser?.uid == nil {
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
         } else {
@@ -28,11 +30,16 @@ class MainScreenController: UITabBarController {
             }, withCancel: nil)
         }
         
-        view.addSubview(tableView)
-        setupTableView()
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.white
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
+        //view.addSubview(tableView)
+        //setupTableView()
+        //view.addSubview(tabBar)
+        //setupTabBar()
     }
     
-    let tableView: UITableView = {
+    
+    /*let tableView: UITableView = {
        let tv = UITableView()
        tv.translatesAutoresizingMaskIntoConstraints = false
        return tv
@@ -42,9 +49,10 @@ class MainScreenController: UITabBarController {
         tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         tableView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        tableView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-    }
-    
+        //tableView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }*/
     
     func handleLogout() {
         
@@ -56,6 +64,21 @@ class MainScreenController: UITabBarController {
         let loginController = LoginController()
         //let loginController = ClassSelectController()
         present(loginController, animated: false, completion: nil)
+    }
+    
+    func handleSettings(){
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection
+        section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "hello")
+        cell.textLabel?.text = "HAHAHAHHA"
+        return cell
     }
 }
 
