0335feddd9164c390200c37bd61dd8b091c742b3
dennishuebert <dennishuebert1@gmail.com>
2016-11-12 10:42:56 -0800

    added settings and compression for profile images

--- StudyCast/MainScreenController.swift
+++ StudyCast/MainScreenController.swift
@@ -11,9 +11,10 @@ import Firebase
 
 class MainScreenController: UITableViewController {
 
+    var userCourses = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: .plain, target: self, action: #selector(handleSettings))
         self.navigationController?.navigationBar.barTintColor = UIColor(r: 61, g: 91, b: 151)
@@ -21,38 +22,50 @@ class MainScreenController: UITableViewController {
         if FIRAuth.auth()?.currentUser?.uid == nil {
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
         } else {
-            let uid = FIRAuth.auth()?.currentUser?.uid
-            FIRDatabase.database().reference().child("users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                if let userDictionary = snapshot.value as?[String: AnyObject] {
-                    self.navigationItem.title = userDictionary["name"] as? String
-                }
-            }, withCancel: nil)
+            fetchNameSetupNavBar()
         }
-        
-        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.white
-        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
-        //view.addSubview(tableView)
-        //setupTableView()
-        //view.addSubview(tabBar)
-        //setupTabBar()
+        userCourses.removeAll()
+        self.tableView.reloadData()
     }
     
+    //Not a really a good fix, but it will do for now.
+    //This is bad because everytime the mainScreen re-opend it reloads all data on that screen
+    //we only need to do this when something changes on that screen such as, a change of classes, logout and login as a 
+    //different user, etc.
+    override func viewWillAppear(_ animated: Bool) {
+        if FIRAuth.auth()?.currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        } else {
+            fetchNameSetupNavBar()
+            fetchClasses()
+        }
+        
+    }
     
-    /*let tableView: UITableView = {
-       let tv = UITableView()
-       tv.translatesAutoresizingMaskIntoConstraints = false
-       return tv
-    }()
+    func fetchNameSetupNavBar() {
+        guard let uid = FIRAuth.auth()?.currentUser?.uid else {
+            return
+        }
+        FIRDatabase.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            if let userDictionary = snapshot.value as?[String: AnyObject] {
+                self.navigationItem.title = userDictionary["name"] as? String
+                let titleDict: NSDictionary = [NSForegroundColorAttributeName: UIColor.white]
+                self.navigationController?.navigationBar.titleTextAttributes = titleDict as? [String : Any]
+                self.navigationItem.rightBarButtonItem?.tintColor = UIColor.white
+                self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
+            }
+        }, withCancel: nil)
+    }
     
-    func setupTableView() {
-        tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        tableView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        //tableView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    }*/
+    func fetchClasses() {
+        userCourses.removeAll()
+        let uid = FIRAuth.auth()?.currentUser?.uid
+        FIRDatabase.database().reference().child("users").child(uid!).child("courses").observe(.childAdded, with: { (snapshot) in
+            self.userCourses.append(snapshot.value as! String)
+            self.tableView.reloadData()
+        })
+    }
     
     func handleLogout() {
         
@@ -67,17 +80,22 @@ class MainScreenController: UITableViewController {
     }
     
     func handleSettings(){
-    
+        let settingsController = SettingsController()
+        let settingsNavController = UINavigationController(rootViewController: settingsController)
+        present(settingsNavController, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection
         section: Int) -> Int {
-        return 5
+        return userCourses.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "hello")
-        cell.textLabel?.text = "HAHAHAHHA"
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cellId")
+        if(userCourses.count > 0) {
+            let course = userCourses[indexPath.row]
+            cell.textLabel?.text = course
+        }
         return cell
     }
 }
