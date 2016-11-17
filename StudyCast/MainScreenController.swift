bef27d66d6fd5524b89cbba2b4275492daf420fe
dennishuebert <dennishuebert1@gmail.com>
2016-11-16 23:20:49 -0800

    added different headers and sections to settings menu

--- StudyCast/MainScreenController.swift
+++ StudyCast/MainScreenController.swift
@@ -22,9 +22,6 @@ class MainScreenController: UITableViewController {
         if FIRAuth.auth()?.currentUser?.uid == nil {
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
         }
-        else {
-            fetchNameSetupNavBar()
-        }
         userCourses.removeAll()
         self.tableView.reloadData()
     }
@@ -49,15 +46,11 @@ class MainScreenController: UITableViewController {
         FIRDatabase.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
             
             if let userDictionary = snapshot.value as? [String: AnyObject] {
-                //self.navigationItem.title = userDictionary["name"] as? String
-                //let titleDict: NSDictionary = [NSForegroundColorAttributeName: UIColor.white]
-                //self.navigationController?.navigationBar.titleTextAttributes = titleDict as? [String : Any]
                 self.navigationItem.rightBarButtonItem?.tintColor = UIColor.white
                 self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
                 
                 let titleView = UIView()
                 titleView.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
-                //titleView.backgroundColor = UIColor.red
                 
                 let containerView = UIView()
                 containerView.translatesAutoresizingMaskIntoConstraints = false
@@ -89,7 +82,7 @@ class MainScreenController: UITableViewController {
                 nameLabel.heightAnchor.constraint(equalTo: profileImageView.heightAnchor).isActive = true
                 
                 nameLabel.textColor = UIColor.white
-                
+        
                 containerView.centerXAnchor.constraint(equalTo: titleView.centerXAnchor).isActive = true
                 containerView.centerYAnchor.constraint(equalTo: titleView.centerYAnchor).isActive = true
                 
@@ -114,8 +107,8 @@ class MainScreenController: UITableViewController {
         } catch let logoutError {
             print(logoutError)
         }
-        //let loginController = LoginController()
-        let loginController = ClassSelectController()
+        let loginController = LoginController()
+        //let loginController = ClassSelectController()
         present(loginController, animated: false, completion: nil)
     }
     
--- StudyCast/SettingsController.swift
+++ StudyCast/SettingsController.swift
@@ -10,35 +10,68 @@ import UIKit
 
 class SettingsController: UITableViewController {
 
+    var headerTitles = ["Profile","Cast","About"]
+    var numberOfRowsInSection = [2,3,1]
+    var settingItems = ["Modify your Account", "Select Different Classes", "In Development", "In Development", "In Development", "Version 1.0.2"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(handleCancel))
         self.navigationController?.navigationBar.barTintColor = UIColor(r: 61, g: 91, b: 151)
         self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
         let titleDict: NSDictionary = [NSForegroundColorAttributeName: UIColor.white]
         self.navigationController?.navigationBar.titleTextAttributes = titleDict as? [String : Any]
         self.navigationItem.title = "Settings"
+        
+        tableView.register(MyCell.self, forCellReuseIdentifier: "cellId")
+        tableView.register(Header.self, forHeaderFooterViewReuseIdentifier: "headerId")
+        
+        tableView.sectionHeaderHeight = 50
     }
 
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return headerTitles.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return numberOfRowsInSection[section]
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! MyCell
+        
+        if indexPath.section == 0 {
+            cell.nameLabel.text = settingItems[indexPath.row]
+        } else if indexPath.section == 1 {
+            cell.nameLabel.text = settingItems[indexPath.row + 2]
+        } else if indexPath.section == 2 {
+            cell.nameLabel.text = settingItems[indexPath.row + settingItems.count - 1]
+        }
+        
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "headerId") as! Header
+        header.nameLabel.text = headerTitles[section]
+        return header
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.section == 0
+        {
+            if indexPath.row == 0 {
+                let modifyAccountController = ModifyAccountController()
+                navigationController?.pushViewController(modifyAccountController, animated: true)
+            } else if indexPath.row == 1 {
+                let classSelectController = ClassSelectController()
+                navigationController?.pushViewController(classSelectController, animated: true)
+            }
+        }
+    }
+ 
 
     /*
     // Override to support conditional editing of the table view.
@@ -84,8 +117,68 @@ class SettingsController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
     func handleCancel() {
         self.dismiss(animated: true, completion: nil)
     }
+
 }
+
+
+class Header: UITableViewHeaderFooterView {
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "My Header"
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews(){
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+    }
+}
+
+
+class MyCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "sample label"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews(){
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+    }
+}
+
+
+
+
+
+
+
+
+
